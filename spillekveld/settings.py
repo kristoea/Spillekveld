ddbfac3ed6356ec87f4cad4c92fb4bd2df9337da
kristoea <15893178+kristoea@users.noreply.github.com>
2021-06-20 17:14:32 +0200

    Use new env variable HTTP_HOST

--- spillekveld/settings.py
+++ spillekveld/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get("WEBSITE_HOSTNAME")]
+ALLOWED_HOSTS = [os.environ.get("HTTP_HOST")]
 
 
 # Application definition
