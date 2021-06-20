ae8d700060ad8730c07eab8efb3eac5989624598
kristoea <15893178+kristoea@users.noreply.github.com>
2021-06-20 19:21:01 +0200

    Make MidgardCon page

--- spillekveld/urls.py
+++ spillekveld/urls.py
@@ -19,5 +19,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name="base.html")),
+    path('midgardcon/', TemplateView.as_view(template_name="midgardcon.html")),
     path('admin/', admin.site.urls),
 ]
