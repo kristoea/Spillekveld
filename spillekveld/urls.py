5b3299235f2611e344500928e4449a4188542bfb
Nohtyp <63245440+mathiashjelmer@users.noreply.github.com>
2026-01-10 13:42:39 +0100

    First commit, changing year

--- spillekveld/urls.py
+++ spillekveld/urls.py
@@ -22,9 +22,9 @@ from theme.views import ProgramView, register_request, login_request, logout_req
 urlpatterns = [
     path('', TemplateView.as_view(template_name="base.html")),
     path('midgardcon/', TemplateView.as_view(template_name="midgardcon.html"), name="midgardcon"),
-    path('midgardcon/program/', ProgramView.as_view(template_name="midgardconprog.html"), name="program"),
     path('midgardcon/skrivekonkurranse/', ProgramView.as_view(template_name="skrivekonkurranse.html"), name="skrivekonkurranse"),
-    path('midgardcon/midgardconishprogram/', ProgramView.as_view(template_name="midgardconishprog.html"), name="midgardconishprogram"),
+    path('midgardcon/midgardconprogram/', ProgramView.as_view(template_name="midgardconprog.html"), name="midgardconprogram"),
+    path('midgardcon/program/', ProgramView.as_view(template_name="midgardconishprog.html"), name="program"),
     path('admin/', admin.site.urls),
     path("midgardcon/registrer/", register_request, name="registrer"),
     path("midgardcon/logginn/", login_request, name="logginn"),
